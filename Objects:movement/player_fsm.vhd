4bb2d9c6a4f96ea2a6872126deeef4d41725feaa
samaanhane98 <44997523+samaanhane98@users.noreply.github.com>
2018-11-27 11:04:32 +0100

    Add files via upload

--- Objects:movement/player_fsm.vhd
+++ Objects:movement/player_fsm.vhd
@@ -10,8 +10,9 @@ ENTITY player is
             --  case    : in  std_logic_vector(? downto 0); voor evt levens etc
             clk         : in  std_logic;
             reset       : in  std_logic;
-            x_position  : out std_logic_vector(7 downto 0);
-            y_position  : out std_logic_vector(7 downto 0)
+		frame_ready : in std_logic;
+            x_position  : out std_logic_vector(8 downto 0);
+            y_position  : out std_logic_vector(8 downto 0)
       );
 end player;
 
@@ -24,7 +25,7 @@ signal new_state_x   : state_fsm_x;
 signal state_x        : state_fsm_x;
 signal new_state_y  : state_fsm_y;
 signal state_y      : state_fsm_y;
-signal x_pos, y_pos : std_logic_vector(7 downto 0);
+signal x_pos, x_pos_new, y_pos, y_pos_new : std_logic_vector(8 downto 0);
 
 begin
   process(clk)
@@ -36,18 +37,21 @@ begin
       else
         state_x <= new_state_x;
 	state_y <= new_state_y;
+	x_position <= x_pos_new;
+	y_position <= y_pos_new;
+	x_pos <= x_pos_new;
+	y_pos <= y_pos_new;
       end if;
     end if;
   end process;
   
-  process(state_x, x_control)
+  process(state_x, x_control, frame_ready)
   begin
 	
 
     case state_x is
     when start_x =>
-      x_pos <= "00111111";
-      x_position <= x_pos;
+      x_pos_new <= "00111111";
       if (x_control = "01") then
         new_state_x <= move_right;
       elsif ( x_control = "10") then
@@ -57,8 +61,9 @@ begin
       end if;
       
     when move_right =>
-      x_pos <= x_pos + "00000001";
-      x_position <= x_pos;
+	
+      x_pos_new <= x_pos + "00000001";
+	
       if (x_control = "01") then
         new_state_x <= move_right;
       elsif ( x_control = "10") then
@@ -68,8 +73,9 @@ begin
       end if;
       
     when move_left =>
-      x_pos <= x_pos - "00000001";
-      x_position <= x_pos;
+	
+      x_pos_new <= x_pos - "00000001";
+	
       if (x_control = "01") then
         new_state_x <= move_right;
       elsif ( x_control = "10") then
@@ -79,8 +85,7 @@ begin
       end if;
       
     when still_x =>
-      x_pos <= x_pos;
-      x_position <= x_pos;
+      x_pos_new <= x_pos;
       if (x_control = "01") then
         new_state_x <= move_right;
       elsif ( x_control = "10") then
@@ -91,13 +96,12 @@ begin
     end case;
   end process;
   
-  process(state_y, y_control)
+  process(state_y, y_control, frame_ready)
   begin
 	
     case state_y is
     when start_y => 
-      y_pos <= "01111111";
-      y_position <= y_pos;
+      y_pos_new <= "01111111";
       if (y_control = "01") then
         new_state_y <= move_up;
       elsif ( y_control = "10") then
@@ -107,8 +111,9 @@ begin
       end if;
       
     when move_up =>
-      y_pos <= y_pos + "00000001";
-      y_position <= y_pos;
+	
+      y_pos_new <= y_pos + "00000001";
+	
       if (y_control = "01") then
         new_state_y <= move_up;
       elsif ( y_control = "10") then
@@ -118,8 +123,9 @@ begin
       end if;
       
     when move_down =>
-      y_pos <= y_pos - "00000001";
-      y_position <= y_pos;
+	
+      y_pos_new <= y_pos - "00000001";
+	
       if (y_control = "01") then
         new_state_y <= move_up;
       elsif ( y_control = "10") then
@@ -129,8 +135,7 @@ begin
       end if;
       
     when still_y => 
-      y_pos <= y_pos;
-      y_position <= y_pos;
+      y_pos_new <= y_pos;
       if (y_control = "01") then
         new_state_y <= move_up;
       elsif ( y_control = "10") then
--- Objects:movement/player_fsm_tb.vhd
+++ Objects:movement/player_fsm_tb.vhd
@@ -13,14 +13,14 @@ component player is
             	--  case    : in  std_logic_vector(? downto 0); voor evt levens etc
             	clk         : in  std_logic;
             	reset       : in  std_logic;
-            	x_position  : out std_logic_vector(7 downto 0);
-            	y_position  : out std_logic_vector(7 downto 0)
+            	x_position  : out std_logic_vector(8 downto 0);
+            	y_position  : out std_logic_vector(8 downto 0)
       	);
 end component player;
 
 signal clk, reset : std_logic;
 signal x_control, y_control : std_logic_vector(1 downto 0);
-signal x_position, y_position : std_logic_vector(7 downto 0);
+signal x_position, y_position : std_logic_vector(8 downto 0);
 
 begin
 
@@ -34,15 +34,24 @@ lb11: player port map (x_control, y_control, clk, reset, x_position, y_position)
 
 	x_control <= 	"00" after 0 ns,
 			"01" after 100 ns,
-			"00" after 150 ns,
-			"10" after 170 ns,
-			"00" after 200 ns;
+			"00" after 500 ns,
+			"10" after 700 ns,
+			"00" after 900 ns,
+			"01" after 1100 ns,
+			"00" after 1300 ns,
+			"10" after 1500 ns,
+			"11" after 1700 ns;
 
 	y_control <= 	"00" after 0 ns,
 			"01" after 100 ns,
-			"00" after 150 ns,
-			"10" after 170 ns,
-			"00" after 200 ns;
+			"00" after 500 ns,
+			"10" after 700 ns,
+			"00" after 900 ns,
+			"10" after 1100 ns,
+			"00" after 1300 ns,
+			"01" after 1500 ns,
+			"11" after 1700 ns;
+
 
 end architecture;
 
