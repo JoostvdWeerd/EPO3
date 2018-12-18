578cc6460a2c5c050afca8a15b37cedd8c2b1d5f
JoostvdWeerd <38658993+JoostvdWeerd@users.noreply.github.com>
2018-12-18 10:10:49 +0100

    Update control_bullet-behaviour.vhd

--- Objects:movement/control_bullet-behaviour.vhd
+++ Objects:movement/control_bullet-behaviour.vhd
@@ -1,5 +1,7 @@
 library IEEE;
 use IEEE.std_logic_1164.ALL;
+use IEEE.std_logic_unsigned.ALL;
+use IEEE.numeric_std.ALL;
 
 architecture behaviour of control_bullet is
 
@@ -7,35 +9,35 @@ begin
 process (bullet_inscreen_1, shoot)
 begin
 
-if  (bullet_inscreen_3 = '0' && bullet_inscreen_2 = '0' && bullet_inscreen_1 = '0' && shoot = '1') then 
+if  (bullet_inscreen_3 = '0' AND bullet_inscreen_2 = '0' AND bullet_inscreen_1 = '0' AND shoot = '1') then 
 	shootb1 <= '1';     
       	shootb2 <= '0';
         shootb3 <= '0';
-elsif (bullet_inscreen_3 = '0' && bullet_inscreen_2 = '0' && bullet_inscreen_1 = '1' && shoot = '1') then
+elsif (bullet_inscreen_3 = '0' AND bullet_inscreen_2 = '0' AND bullet_inscreen_1 = '1' AND shoot = '1') then
 	 shootb1 <= '0';     
          shootb2 <= '1';
         	shootb3 <= '0';
-elsif (bullet_inscreen_3 = '0' && bullet_inscreen_2 = '1' && bullet_inscreen_1 = '0' && shoot = '1') then
+elsif (bullet_inscreen_3 = '0' AND bullet_inscreen_2 = '1' AND bullet_inscreen_1 = '0' AND shoot = '1') then
 	 shootb1 <= '1';     
          shootb2 <= '0';
         	shootb3 <= '0';
-elsif (bullet_inscreen_3 = '0' && bullet_inscreen_2 = '1' && bullet_inscreen_1 = '1' && shoot = '1') then
+elsif (bullet_inscreen_3 = '0' AND bullet_inscreen_2 = '1' AND bullet_inscreen_1 = '1' AND shoot = '1') then
 	 shootb1 <= '0';     
          shootb2 <= '0';
         	shootb3 <= '1';
-elsif (bullet_inscreen_3 = '1' && bullet_inscreen_2 = '0' && bullet_inscreen_1 = '0' && shoot = '1') then
+elsif (bullet_inscreen_3 = '1' AND bullet_inscreen_2 = '0' AND bullet_inscreen_1 = '0' AND shoot = '1') then
 	 shootb1 <= '1';     
          shootb2 <= '0';
         	shootb3 <= '0';
-elsif (bullet_inscreen_3 = '1' && bullet_inscreen_2 = '0' && bullet_inscreen_1 = '1' && shoot = '1') then
+elsif (bullet_inscreen_3 = '1' AND bullet_inscreen_2 = '0' AND bullet_inscreen_1 = '1' AND shoot = '1') then
 	 shootb1 <= '0';     
          shootb2 <= '1';
         	shootb3 <= '0';
-elsif (bullet_inscreen_3 = '1' && bullet_inscreen_2 = '1' && bullet_inscreen_1 = '0' && shoot = '1') then
+elsif (bullet_inscreen_3 = '1' AND bullet_inscreen_2 = '1' AND bullet_inscreen_1 = '0' AND shoot = '1') then
 	 shootb1 <= '1';     
          shootb2 <= '0';
         	shootb3 <= '0';
-elsif (bullet_inscreen_3 = '1' && bullet_inscreen_2 = '1' && bullet_inscreen_1 = '1' && shoot = '1' then
+elsif (bullet_inscreen_3 = '1' AND bullet_inscreen_2 = '1' AND bullet_inscreen_1 = '1' AND shoot = '1') then
 	 shootb1 <= '0';     
          shootb2 <= '0';
         	shootb3 <= '0';
@@ -50,4 +52,3 @@ end if;
 end process;
 
 end behaviour;
-
