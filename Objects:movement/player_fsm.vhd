9cb09892fdefbe48b46027c1f14f8627ed04b978
JoostvdWeerd <38658993+JoostvdWeerd@users.noreply.github.com>
2018-12-18 10:08:17 +0100

    Update player_fsm.vhd

--- Objects:movement/player_fsm.vhd
+++ Objects:movement/player_fsm.vhd
@@ -10,13 +10,13 @@ ENTITY player is
             --  case    : in  std_logic_vector(? downto 0); voor evt levens etc
             clk         : in  std_logic;
             reset       : in  std_logic;
-		frame_ready : in std_logic;
+	    frame_ready : in std_logic;
             x_position  : out std_logic_vector(8 downto 0);
             y_position  : out std_logic_vector(8 downto 0)
       );
 end player;
 
-ARCHITECTURE fsm of player is
+ARCHITECTURE behavior of player is
   
   type state_fsm_x is ( start_x, move_right, move_left, still_x);
   type state_fsm_y is ( start_y, move_up, move_down, still_y);
@@ -37,10 +37,6 @@ begin
       else
         state_x <= new_state_x;
 	state_y <= new_state_y;
-	x_position <= x_pos_new;
-	y_position <= y_pos_new;
-	x_pos <= x_pos_new;
-	y_pos <= y_pos_new;
       end if;
     end if;
   end process;
@@ -51,7 +47,7 @@ begin
 
     case state_x is
     when start_x =>
-      x_pos_new <= "00111111";
+      x_pos_new <= "000111111";
       if (x_control = "01") then
         new_state_x <= move_right;
       elsif ( x_control = "10") then
@@ -93,6 +89,11 @@ begin
       else 
         new_state_x <= still_x;
       end if;
+
+    when others =>
+	new_state_x <= start_x;
+	x_pos_new <= "000111111";
+
     end case;
   end process;
   
@@ -101,7 +102,7 @@ begin
 	
     case state_y is
     when start_y => 
-      y_pos_new <= "01111111";
+      y_pos_new <= "001111111";
       if (y_control = "01") then
         new_state_y <= move_up;
       elsif ( y_control = "10") then
@@ -143,6 +144,16 @@ begin
       else 
         new_state_y <= still_y;
       end if;
+
+    when others =>
+	new_state_y <= start_y;
+	y_pos_new <= "001111111";
+
     end case;
   end process;
-end architecture fsm;
+	x_position <= x_pos_new;
+	y_position <= y_pos_new;
+	x_pos <= x_pos_new;
+	y_pos <= y_pos_new;
+
+end architecture;
